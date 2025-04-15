6d767553a592e6639c1cc870ed6794c9ac9beb6f
Rachel Mae Almonte <rachel.mae.almonte@accenture.com>
2025-04-15 14:39:07 +0800

    redeploy new structure folder without the myexpressapp folder

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; // test redeploy dummy commit
 
 app.listen(port, () => {
   console.log(`Express app is running on port ${port}`);
